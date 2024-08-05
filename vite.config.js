8681357b8f8ed54f97860100b93bf4a214e2b486
adykrgupta <114936957+adykrgupta@users.noreply.github.com>
2024-08-05 14:01:02 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deploy-github/",
+  base: "/index.html",
   plugins: [react()],
   css:{
     modules:{
